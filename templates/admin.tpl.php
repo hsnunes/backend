920ed5f5e91b508ec464978236d5aef80fdfd8a0
Hilder Nunes <hildersn@gmail.com>
2020-09-03 12:51:15 -0300

    Template Modelo de Administraçao - Exemplo

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -6,17 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/dashboardModel.css">
 
     <title>Painel Administrativo</title>
   </head>
   <body>
-    <?php include $content; ?>
+
+<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">
+    HsNnunes
+  </a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <input class="form-control form-control-dark w-100" type="text" placeholder="Procurar" aria-label="Search">
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+      <a class="nav-link" href="#">Sair</a>
+    </li>
+  </ul>
+</nav>
+
+<?php include $content; ?>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="/js/jquery-3.5.1.slim.min.js"></script>
     <script src="/js/popper.js@1.16.1_dist_umd_popper.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
+    <!-- Js Extras -->
+    <script src="/js/feather-icons_4.9.0.min.js"></script>
+    <script src="/js/Chart_2.7.3.min.js"></script>
+    <script src="/js/dashboard.js"></script>
+
   </body>
 </html>
