b31c39fe5cc4447e9f78f359996fefdcd077994d
Hilder Nunes <hildersn@gmail.com>
2020-09-05 07:29:23 -0300

    Funcionalidade criada e disponibilizada na app

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,7 @@ require __DIR__ . '/src/error_handler.php';
 require __DIR__ . '/src/connection.php';
 require __DIR__ . '/src/resolve-router.php';
 require __DIR__ . '/src/render.php';
+require __DIR__ . '/src/flash.php';
 
 // Verifica as rotas do usuário e encaminha para os diretorios seguintes
 if (resolve('/admin/?(.*)')) {
